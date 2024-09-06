0fa2e301c0cdb68ee6e0140595b08fd7e6053b57
Aaron N. <aaronjn78@yahoo.com>
2024-09-06 15:36:16 -0400

    Updated app with basic, but ugly GUI and all functionality added

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-## Temperature converter
+# Temperature converter
 Basic app to automatically convert from one unit of temperature to the other two units automtically
 
-### Goals
+## Goals
 - Practice creating basic GUIs using Java AWT
 - Practice utilizing event handling techniques
 - Practice error handling
+
+## Steps/Updates
+
+1. Created basic GUI with text fields and labels
+	- considered implementing use of buttons, either a single convert button to trigger the conversion, or multiple buttons to trigger the conversion from the changed unit to the to the other two. Decided to implement trigger the conversions through typing events.
+2. Added methods to convert between different temperature units
+3. Added event listeners to text fields to dynamically check to changes to the text and call conversion methods
+4. Looked up and implemented a method to close the application cleanly
+
+### Bugs/Fixes
+1. Deleting the text from any of the fields resulted in an "Empty String" message being displayed in the console. Attempting to correct this so no console message displayed.
+	- Fix 1: check for empty string using string comparison and return a default string value
+	
+2. Deleting text in Farenheit or Celsius text field still resulting in "Empty String" message being displayed to console. Does not occur in other text fields.
+	- Fix 2: Found the message in the console was an error message when trying to find the double value of the string text. Removed previous fix and added code to check for empty string arguments
+	
+3. Code does not handle non-numeric or blank entries currently. It still runs, but will generate console error messages
+
+	
--- src/TempConverter.java
+++ src/TempConverter.java
@@ -1,86 +1,170 @@
 import java.awt.*;
+import java.awt.event.*;
 
-public class TempConverter {
+public class TempConverter extends KeyAdapter{
 
-    public TempConverter(){
-        Frame f1 = new Frame("Temperature Converter");
-        //for some reason was considering using multiple buttons for a user to click in order to trigger the event to calculate the other temperatures. 
-        
+	/**
+	 * 
+	 */
+	public TempConverter(){
+		Frame f1 = new Frame("Temperature Converter");
+
+		/*Considered using buttons for user to click to trigger conversion from
+		 * associated temperature unit to other units. Then considered having a single convert
+		 * buttong. Then decided to just trigger the conversion dynamically based on changing
+		 * text in each field.  
+
         //Button btnFarenheit = new Button("Farenheit");
         //Button btnCelsius = new Button("Celsius");
         //Button btnKelvinButton = new Button("Kelvin");
-       // Button btnConvert = new Button("Convert");
-        TextField tfFarenheit = new TextField("32");
-        TextField tfCelsius = new TextField("0");
-        TextField tfKelvin = new TextField("273.15");
-        Label lblFarenheit = new Label("°F", Label.LEFT);
-        Label lblCelcius = new Label("°C", Label.LEFT);
-        Label lblKelvin = new Label("°F", Label.LEFT);
-        
+        //Button btnConvert = new Button("Convert");
+		 */
+		TextField tfFarenheit = new TextField("32");
+		TextField tfCelsius = new TextField("0");
+		TextField tfKelvin = new TextField("273.15");
+		Label lblFarenheit = new Label("°F", Label.LEFT);
+		Label lblCelcius = new Label("°C", Label.LEFT);
+		Label lblKelvin = new Label("°K", Label.LEFT); 
 
-        tfFarenheit.setBounds(40, 50, 100, 25);
-        tfCelsius.setBounds(40, 85, 100, 25);
-        tfKelvin.setBounds(40, 120, 100, 25);
+		//event listeners for text fields that trigger on key releases to ensure the text is read after the key is pressed, not before
+		tfFarenheit.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e){
 
-  
-        f1.add(tfFarenheit,0);
-        f1.add(tfCelsius,1);
-        f1.add(tfKelvin,2);
+				tfCelsius.setText(FarenheitToCelcius(tfFarenheit.getText()));
+				tfKelvin.setText(CelsiustoKelvin(tfCelsius.getText()));
+			}
+		});
 
-        f1.setSize(300, 300);
-        f1.setBackground(Color.lightGray);
-        f1.setLayout(null);
-        f1.setVisible(true);
+		tfCelsius.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e){
 
-    }
+				tfFarenheit.setText(CelsiustoFarenheit(tfCelsius.getText()));
+				tfKelvin.setText(CelsiustoKelvin(tfCelsius.getText()));
+			}
+		});
 
-    private String FarenheitToCelcius(String Farenheit){
-        
-        double C = 0;
-        try {
-            double F = Double.valueOf(Farenheit);
-            C = (F - 32) * (5/9);
+		tfKelvin.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e){
 
-        } catch (NumberFormatException e) {
-            //need to write error message popup at some point
-            System.err.println(e.getMessage());
-        }
-        return Double.toString(C);
-    }
+				tfCelsius.setText(KelvintoCelcius(tfKelvin.getText()));
+				tfFarenheit.setText(CelsiustoFarenheit(tfCelsius.getText()));
+			}
+		});
 
-    private String CelsiustoFarenheit(String Celsius){
-        
-        double F = 32;
-        try {
-            double C = Double.valueOf(Celsius);
-             F = (C *(5/9)) + 32;
+		//Window event listener to close the application
+		f1.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				f1.dispose();
+			};
+		});
 
-        } catch (NumberFormatException e) {
-            //need to write error message popup at some point
-            System.err.println(e.getMessage());
-        }
-        return Double.toString(F);
-    }
-
-    private String CelsiustoKelvin(String Celsius){
-        double K = 273.15;
-        try {
-            double C = Double.valueOf(Celsius);
-            K = C + 273.15;
-            
-        } catch (NumberFormatException e) {
-            System.err.println(e.getMessage());
-            //need to write popup
-        }
-        return Double.toString(K);
-
-    }
+		//set size and positions of components in frame
+		tfFarenheit.setBounds(40, 50, 100, 25);
+		lblFarenheit.setBounds(141, 50, 50, 25);
+		tfCelsius.setBounds(40, 85, 100, 25);
+		lblCelcius.setBounds(141, 85, 50, 25);
+		tfKelvin.setBounds(40, 120, 100, 25);
+		lblKelvin.setBounds(141, 120, 50, 25);
 
 
-    public static void main(String[] args) throws Exception {
-        new TempConverter();
-        //need to learn how to exit out of GUI by clicking x
+		f1.add(tfFarenheit,0);
+		f1.add(lblFarenheit);
+		f1.add(tfCelsius,1);
+		f1.add(lblCelcius);
+		f1.add(tfKelvin,2);
+		f1.add(lblKelvin);
 
-    }
+		f1.setSize(300, 300);
+		f1.setBackground(Color.lightGray);
+		f1.setLayout(null);
+		f1.setVisible(true);
+	}
+
+
+	private String FarenheitToCelcius(String Farenheit){
+
+		if (Farenheit.isBlank()) return "";
+		double C = 0;
+		try {
+			//only convert if there is a Farenheit isn't blank
+			//if(!Farenheit.isBlank()) {
+			double F = Double.valueOf(Farenheit);
+			C = (F - 32) * (5.0/9.0);
+			//}
+
+		} catch (NumberFormatException e) {
+			//need to write error message popup at some point
+			System.err.println(e.getMessage());
+		}
+
+		return Double.toString(C);
+	}
+
+
+	private String CelsiustoFarenheit(String Celsius){
+
+		if (Celsius.isBlank()) return "";
+		double F = 0;
+		try {
+			//if (!Celsius.isBlank()) {
+			double C = Double.valueOf(Celsius);
+			F = (9.0/5.0) * C + 32;
+			//}
+
+		} catch (NumberFormatException e) {
+			//need to write error message popup at some point
+			System.err.println(e.getMessage());
+		}
+
+		return Double.toString(F);
+	}
+
+
+	private String CelsiustoKelvin(String Celsius){
+
+		if (Celsius.isBlank()) return "";
+		double K = 273.15;
+		try {
+			//if (!Celsius.isBlank()){
+			double C = Double.valueOf(Celsius);
+			K = C + 273.15;
+			//}
+
+		} catch (NumberFormatException e) {
+			System.err.println(e.getMessage());
+			//need to write popup
+		}
+
+		return Double.toString(K);
+	}
+
+
+	private String KelvintoCelcius(String Kelvin){
+
+		//catch empty string values when deleting values and return 0
+		if (Kelvin.isBlank()) return "";
+
+		double C = 0;
+		try {
+			double K = Double.valueOf(Kelvin);
+			C = K - 273.15;
+		}
+		catch (NumberFormatException e){
+			System.err.println(e.getMessage());
+		}
+
+		//if (Double.toString(C).isEmpty()) {
+		//	return "0";
+		//}
+		return Double.toString(C);
+
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		new TempConverter();
+		//need to learn how to exit out of GUI by clicking x
+
+	}
 
 }
